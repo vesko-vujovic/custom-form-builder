34cf343836dbd0773e82d9956d85128009ed3f14
Vesko Vujovic <veskovujovic@yahoo.com>
2015-08-01 22:47:58 +0200

    imported angular-sort
    angular sort works fine with angular draggable  widget

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Angular files for  custom-property builder
  */
 
-var customForm = angular.module('form-builder', []);
+var customForm = angular.module('form-builder', ['ui.sortable']);
 
 
 // Loads pallete of available fields, and html templates for that fields
@@ -57,11 +57,7 @@ customForm.directive('formfield', ['$compile', '$templateCache', function($compi
         template: '<ng-include src="' + 'getTemplateUrl()' +'"/>'
          ,
         link: function(scope, element, attribute){
-
-        
-            console.log(scope.fieldData);
-        
-
+    
             scope.getTemplateUrl = function(type){
                 return "input.html";
             }
@@ -105,6 +101,8 @@ customForm.controller('IndexController',  ['$scope', 'getDataForCustomForm', '$t
 
         if($scope.initialCi.length !== 0 ){
             $scope.tempStorageForObjects = $scope.initialCi.groups;
+            // Find ondex of object where we have form property
+
         }
     });
 
@@ -124,104 +122,13 @@ customForm.controller('IndexController',  ['$scope', 'getDataForCustomForm', '$t
         $scope.templates  = data.templates;
     });
 
-
-    // Initialize Jquery UI widgets on the page
-    $scope.startWidgets    = function(){
-    
-        $("#sortable").sortable({
-            revert: true,
-            update: function(event, ui){
-               var index = ui.item.index();
-               $scope.relaceDraggedElement(ui);
-            }
-        });
-
-        // List draggable fields on the right
-        $scope.listDraggableFields();
-
-    };
-
-    // Initialize draggable widget - this function will be called from $scope.listDraggableFields();
-    $scope.initializeDrag   = function() {
-        $('.drop li').draggable({
-            connectToSortable: '#sortable',
-            helper: 'clone',
-            revert: 'invalid',
-            start: function( event, ui ) {
-
-            }
-        });
-    }
-
-    // Replace dragged element on the right with adeuquate element
-    $scope.relaceDraggedElement   = function(draggedElement){
-
-            var typeOfField           = draggedElement.item.text();
-            var currentElement        = draggedElement.item;
-            var indexOfDraggedElement = draggedElement.item.index();
-            console.log(indexOfDraggedElement);
-        //
-        switch (typeOfField){
-            case 'input':
-                // @param {object} - Create object for this dragged field
-                var obj = $scope.createObjectForElement(indexOfDraggedElement, typeOfField);
-                $(currentElement).replaceWith(
-                    '<li data-type="' + obj.field_type + '" data-ref="' + obj.ref + '">' +
-                    '<label>' + obj.key + '</label><input type="text" disabled=true>' +
-                    '</li>'
-                );
-                $scope.tempStorageForObjects.push(obj);
-                console.log(obj);
-                break;
-            case 'input-number':
-                var obj = $scope.createObjectForElement(indexOfDraggedElement, typeOfField);
-                $(currentElement).replaceWith(
-                    '<li data-type="' + obj.field_type + '" data-ref="' + obj.ref + '">' +
-                    '<label>' + obj.key + '</label><input type="number" disabled=true>' +
-                    '</li>'
-                );
-                $scope.tempStorageForObjects.push(obj);
-                break;
-            case 'input-decimal':
-                var obj = $scope.createObjectForElement(indexOfDraggedElement, typeOfField);
-                $(currentElement).replaceWith(
-                    '<li data-type="' + obj.field_type + '" data-ref="' + obj.ref + '">' +
-                    '<label>' + obj.key + '</label><input type="number" set="any" disabled=true>' +
-                    '</li>'
-                );
-                $scope.tempStorageForObjects.push(obj);
-                break;
-
-
-        }
-
-
-    };
-
-
-    // Watch when $scope.pallete is loaded then render left side field types
-    $scope.listDraggableFields = function(){
-
-        $scope.$watchCollection('pallete', function (newValue, oldValue) {
-
-            if(newValue.length !== 0){
-                for( var i = 0; i < $scope.pallete.length; i++) {
-                    $(".drop").append('<li>' + $scope.pallete[i].type + '</li>');
-                };
-            }
-            // After changing the DOM initialize draggable JqueryUI
-            $scope.initializeDrag();
-        });
-
-
-
-    };
-
     // Create object for new dragged element
     $scope.createObjectForElement = function(index, typeOfField){
+          
 
           var timestamp       =  Date.now();
           var dropdownOptions =  "";
+          //console.log(index);
 
           // If typeOfField is dropdown, make initial data for that element
           typeOfField == "dropdown" ? dropdownOptions = [{"state_id": 0, "name": "first_choice", "is_default": true, "deleted": false }] : dropdownOptions = [];
@@ -246,12 +153,41 @@ customForm.controller('IndexController',  ['$scope', 'getDataForCustomForm', '$t
           return obj;
     };
 
+
+    $scope.sortableOptions = {
+        update: function(e, ui) {
+
+            console.log(ui);
+        }
+    };
+
    
 
     $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+       
+      /*  $(".sortable").sortable({
+            revert: true,
+            update: function(event, ui){
+               var index        = ui.item.index();
+               var typeOfField  = ui.item.text();
+               $scope.createObjectForElement(index, typeOfField);
+               console.log(typeOfField);
+               
+            }
+        });   */
+          
+        $('.drop li').draggable({
+            connectToSortable: '.sortable',
+            helper: 'clone',
+            revert: 'invalid',
+            start: function( event, ui ) {
 
+            }
+        });
+
+       
          // Start the draggable and sortable widgets when rendering finishes
-         $scope.startWidgets();
+         //$scope.startWidgets();
     });
 
 
@@ -265,7 +201,7 @@ customForm.controller('IndexController',  ['$scope', 'getDataForCustomForm', '$t
 
 // Cache templates
 customForm.run(function($templateCache){
-    $templateCache.put('input.html', '<label> Untitled <\/label> <div> <input type=\"text\" disabled=\"true\" > <\/div>');
+    $templateCache.put('input.html', '<label> Untitled {{ fieldData.field_type }} <\/label> <div> <input type=\"text\" disabled=\"true\" > <\/div>');
     /*$templateCache.put('input-number.html', '<label> Untitled <\/label> <div> <input type=\"number\" disabled=\"true\" > <\/div>');
     $templateCache.put('input-decimal.html', '<label> Untitled  <\/label> <div> <input type=\"Number\" disabled=\"true\" step=\"any\"> <\/div>');
     $templateCache.put('textarea.html', '<label> Untitled  <\/label> <div> <textarea disabled=\"true\"><\/textarea> <\/div>');
